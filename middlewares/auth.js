edc7727db6650a648577c225b1b3743d9c1d7836
Miro Dojkic <mdojkic@gmail.com>
2017-03-28 10:18:58 +0200

    Add middleware to intercept non staff and forbid access to sg dashboard

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 const { baseUrl, lmsPort } = require('../config/main');
 
-module.exports.getUserInfo = (req, res) => {
+const getUserInfo = (req, res) => {
   const accessToken = req.session.token.access_token;
   const options = {
     method: 'GET',
@@ -18,3 +18,29 @@ module.exports.getUserInfo = (req, res) => {
   })
   .catch(error => res.send(`Access Token Error ${error.message}`));
 };
+
+const interceptNonStaff = (req, res, next) => {
+  const accessToken = req.session.token && req.session.token.access_token;
+  const options = {
+    method: 'GET',
+    url: `${baseUrl}:${lmsPort}/api/user/v1/is_staff`,
+    headers: {
+      Authorization: `Bearer ${accessToken}`
+    }
+  };
+
+  axios(options)
+  .then(response => {
+    const isStaff = response.data;
+
+    if (isStaff) {
+      next();
+    } else {
+      res.status(403);
+      res.send('Access is forbidden for non-staff user!');
+    }
+  })
+  .catch(error => next(error));
+};
+
+module.exports = { getUserInfo, interceptNonStaff };
