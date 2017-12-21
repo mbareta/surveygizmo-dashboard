3a6b4764d1ce60498c4f5750e04a63a23f954199
mbareta <mbareta@extensionengine.com>
2017-12-21 12:08:15 +0100

    Update app.js

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const users = require('./routes/users');
 const responses = require('./routes/responses');
 const { requiresStaffRole } = require('./middlewares/auth');
 
-//TESTING JENKINS
+//TESTING JERKINS
 
 const app = express();
 
