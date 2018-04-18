8c9b3e561581fe082d86c400f071e6a45890d64c
Marin Bareta <mbareta@extensionengine.com>
2018-04-18 14:50:27 +0200

    KF-710: Bugfix - save only allowed fields in DB

--- config/main.js
+++ config/main.js
@@ -1,12 +1,15 @@
 const env = require('./config.json');
+const allowedResponseKeys = require('./allowedResponseKeys');
 
 module.exports = (() => {
-    const node_env = process.env.NODE_ENV || 'development';
-    const env_variables = env[node_env];
+  const node_env = process.env.NODE_ENV || 'development';
+  const env_variables = env[node_env];
 
-    // OAuth2
-    env_variables.client.id = process.env.CLIENT_ID || env_variables.client.id;
-    env_variables.client.secret = process.env.CLIENT_SECRET || env_variables.client.secret;
+  // OAuth2
+  env_variables.client.id = process.env.CLIENT_ID || env_variables.client.id;
+  env_variables.client.secret = process.env.CLIENT_SECRET || env_variables.client.secret;
 
-    return env_variables;
+  env_variables.allowedResponseKeys = allowedResponseKeys;
+
+  return env_variables;
 })();
